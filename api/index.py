c2b14a593854500bf7517998395695a0bc347709
Kaveh Pouran Yousef <kaweyo@gmail.com>
2024-12-06 16:32:19 +0100

    Added new files.

---
+++ api/index.py
@@ -0,0 +1,22 @@
+﻿from fastapi import FastAPI
+
+ENGINEER_ROLES = [
+    {'title': 'Frontend Developer', 'mainskill': 'React'},
+    {'title': 'Backend Developer', 'mainskill': 'Node.js'},
+    {'title': 'Fullstack Developer', 'mainskill': 'Next.js'},
+    {'title': 'Machine Learning Engineer', 'mainskill': 'Tensorflow'},
+    {'title': 'Data Scientist', 'mainskill': 'Apache Spark'},
+    {'title': 'Software Architect', 'mainskill': 'System Analysis'},
+]
+
+# Create FastAPI instance with custom docs and openapi url
+app = FastAPI(docs_url="/api/py/docs",  openapi_url="/api/py/openapi.json")
+
+
+@app.get("/api/py/engineer-roles")
+async def read_category_by_query(title: str): 
+    role_to_return = None
+    for role in ENGINEER_ROLES:
+        if role.get('title').casefold() == title.casefold():
+            role_to_return = role
+    return role_to_return
